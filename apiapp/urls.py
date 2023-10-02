abf26faff99d4bfbc4710f6b2eff7c7bcb16c5ed
Sunny14578 <imsun0101@naver.com>
2023-10-02 19:58:54 +0900

    [edit]:user url update

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import JoinApiView, AuthAPIView, Create_Company, LectureAPIView
+from .views import JoinApiView, AuthAPIView, LectureAPIView, Create_Company, TeacherAPIView
 
 urlpatterns = [
     path('join/user/', JoinApiView.as_view()),
+    path('teacher/', TeacherAPIView.as_view()),
     path('login/', AuthAPIView.as_view()),
     path('logout/', AuthAPIView.as_view()),
-    path('join/company/', Create_Company, name='create_company'),
-    path('lecture/', LectureAPIView.as_view(), name='create_Lecture')
+    path('join/company/', Create_Company, name='Create_Company'),
+    path('lecture/', LectureAPIView.as_view(), name='Lecture_Room')
 ]
