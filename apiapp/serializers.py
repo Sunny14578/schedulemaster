bf53bdeb0ded891f4bd38628856427454fdd4105
Sunny14578 <imsun0101@naver.com>
2023-10-03 20:05:53 +0900

    [edit]:schedulecell serializer add

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Company, LectureRoom
+from .models import User, Company, LectureRoom, ScheduleCell
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,8 +22,13 @@ class UserSerializer(serializers.ModelSerializer):
             name = validated_data['name'],
             password = validated_data['password'],
             phone_number = validated_data['phone_number'],
-            color = "#000000",
+            color = validated_data['color'],
             role = validated_data['role'],
             company_id = validated_data['company_id']
         )
         return user
+    
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduleCell
+        fields = '__all__'
