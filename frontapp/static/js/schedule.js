acbaa8779e63c708f45745ec80d24c1a445bad67
Sunny14578 <imsun0101@naver.com>
2023-10-06 00:26:27 +0900

    [edit]:cell get html add

--- frontapp/static/js/schedule.js
+++ frontapp/static/js/schedule.js
@@ -1,3 +1,5 @@
+var indexTime = ["08:00:00","09:00:00","10:00:00","11:00:00","12:00:00","13:00:00","14:00:00","15:00:00","16:00:00","17:00:00"];
+var bgcolor = ["#5F00FF", "#003399", "#FF5E00", "#22741C", "#F15F5F", "#D941C5", "#5D5D5D", "#664B00", "#5F00FF", "#003399", "#FF5E00", "#22741C"];
 const body = document.querySelector("body"),
     sidebar = body.querySelector(".sidebar"),
     toggle = body.querySelector(".toggle"),
@@ -13,10 +15,16 @@ const body = document.querySelector("body"),
     lecture_modal = body.querySelector("#lecture_modal"),
     lecture_add_btn = body.querySelector(".bx.bx-user-plus.toggle.room"),
     lecture_exit = body.querySelector(".exit.room"),
-    lecture_room_menu = body.querySelector("#sidebar_room .menu-links");
+    lecture_room_menu = body.querySelector("#sidebar_room .menu-links"),
+
+    teacher_menu = body.querySelector("#sidebar_teacher .menu-links"),
+    table_lecture_room = body.querySelector(".lecture_room"),
+    table = body.querySelector(".cell-table"),
+    main_body = body.querySelector(".home.table-menu");
     
     toggle.addEventListener("click", () =>{
         sidebar.classList.toggle("close");
+        
         sidebar22.forEach((side, index2) => {
             side.classList.toggle("close2");
         })
@@ -40,14 +48,27 @@ const body = document.querySelector("body"),
         lecture_modal.classList.toggle("hidden");
     })   
 
+    let preNavindex = null;
     
     sidebar_nav_a.forEach((nav, index) => {
         nav.addEventListener("click", () => {
-
+            
+            if (preNavindex === nav){
+                main_body.classList.toggle("close");
+                preNavindex = null;
+            }else{
+                if(preNavindex){
+                    preNavindex = nav;
+                }else{
+                    main_body.classList.toggle("close");
+                    preNavindex = nav;
+                }
+            }
             nav.classList.toggle("close");
 
             sidebar_nav_a.forEach((nav2, index2) => {
                 if ( index != index2){
+                    
                     nav2.className = "nav a close"
                 }
                 
@@ -59,6 +80,10 @@ const body = document.querySelector("body"),
             
 
             sidebar22.forEach((side, index2) => {
+                if (index == 0){
+                    onDataTeacherGet();
+                }
+
                 if (index == 1){
                     onDataLectureGet();
                 }
@@ -208,9 +233,14 @@ function onDataLecturePost() {
         }
     });
 
+    window.onload = function(){
+        onDataLectureRoomGet();
+        // onCreateCell();
+        onDataCellGet();
+    }
+
 
 function onDataLectureGet(){
-    console.log(1);
     const apiUrl = '/api/lecture/';
 
     fetch(apiUrl, {
@@ -238,6 +268,218 @@ function onDataLectureGet(){
             new_litag.appendChild(new_atag);
             new_atag.appendChild(new_span);
             lecture_room_menu.appendChild(new_litag);
+        }
+    })
+    .catch(error => {
+        console.error('에러 발생:', error);
+    });
+}
+
+function onDataTeacherGet(){
+    const apiUrl = '/api/teacher/';
+
+    fetch(apiUrl, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        teacher_menu.innerHTML = '';
+        const teacherNames = data.map(item => item.name);
+        const colors = data.map(item => item.color)
+
+        for (var i=0; i<teacherNames.length; i++){
+            var new_litag = document.createElement("li");
+            new_litag.className = "nav_link teacher";
+
+            var new_atag = document.createElement("a");
+            new_atag.className = "nav a close";
+
+            var new_span = document.createElement("span");
+            new_span.className = "text nav-text name";
+            new_span.textContent = teacherNames[i];
+
+            var new_input = document.createElement("input");
+            new_input.className = "nav-color color";
+            new_input.type = "color"
+            new_input.value = colors[i];
+
+
+            new_litag.appendChild(new_atag);
+            new_atag.appendChild(new_span);
+            new_atag.appendChild(new_input);
+            teacher_menu.appendChild(new_litag);
+        }
+    })
+    .catch(error => {
+        console.error('에러 발생:', error);
+    });
+}
+
+function onDataLectureRoomGet(){
+    const apiUrl = '/api/lecture/';
+
+    fetch(apiUrl, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        table_lecture_room.innerHTML = '';
+        const roomNames = data.map(item => item.room_name);
+        const roomIdNames = data.map(item => item.room_id);
+
+        localStorage.setItem('roomIdNames', roomIdNames);
+
+        for (var i=0; i<roomNames.length; i++){
+            var new_litag = document.createElement("li");
+            new_litag.contentEditable = true;
+            new_litag.textContent = roomNames[i];
+
+            table_lecture_room.appendChild(new_litag);
+        }
+    })
+    .catch(error => {
+        console.error('에러 발생:', error);
+    });
+}
+
+function onCreateCell(lecture_room_id){
+
+    const apiUrl = '/api/schedule/'; 
+
+    var monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    data_group = [] // post 요청을 줄이기위해 데이터 그룹화
+
+    for (let index = 0; index < indexTime.length; index++){
+        for (let day = 1; day <= monthsDays[0]; day++){
+            data = {
+                "lecture_room_id":6,
+                "day":day,
+                "month":1,
+                "time":indexTime[index]
+            }
+
+        data_group.push(data);
+        }
+    }
+    
+    postData(data_group);
+}
+
+async function postData(dataList) {
+    const apiUrl = '/api/schedule/';
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataList), 
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log('데이터 전송 성공:', responseData);
+        } else {
+            console.error('데이터 전송 실패:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('에러 발생:', error);
+    }
+}
+
+function onDataCellGet(){
+    const apiUrl = '/api/schedule/';
+
+    fetch(apiUrl, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        var monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        const rooms = localStorage.getItem('roomIdNames');
+        const splitRooms = rooms.split(',');
+        const months = data.map(item => item.month);
+        const maxMonth = Math.max(...months);
+        let thead;
+
+        for (let i = 0; i < splitRooms.length; i++){
+            const FilterData = data.filter(item => item.lecture_room_id == splitRooms[i]);
+            
+            if (FilterData.length > 0){
+                if (i%2==0){
+                    thead = document.createElement('thead');
+                    const theadRowFragment = document.createDocumentFragment();
+                    const FilterRowData = FilterData.filter(item => item.time == indexTime[0]);
+                    const cols = FilterRowData.length;
+
+                    const tr = document.createElement('tr');
+                    
+                    for (let j = 0; j < cols; j++){
+                        const td = document.createElement('td');
+                        td.textContent = FilterRowData[j].day;
+                        theadRowFragment.appendChild(td);
+                    }
+                    
+                
+                    const tr2 = document.createElement('tr');
+                    const theadRowFragment2 = document.createDocumentFragment();
+                    
+                    for (let m = 0; m < maxMonth; m++){
+                        const td2 = document.createElement('td');
+                        td2.textContent = m+1+"월";
+                        td2.setAttribute('colspan', monthsDays[m]);
+                        theadRowFragment2.appendChild(td2);
+                        tr.style.backgroundColor = bgcolor[m];
+                        tr2.style.backgroundColor = bgcolor[m];
+                    }
+                    tr.appendChild(theadRowFragment);
+                    thead.appendChild(tr)
+                    tr2.appendChild(theadRowFragment2);
+                    thead.appendChild(tr2);
+                    table.appendChild(thead);
+                }
+                
+                const tbody = document.createElement('tbody');
+                tbody.setAttribute('id', rooms[i]+"tbody");
+
+                for (let i = 0; i < 10; i++){
+                    const tr = document.createElement('tr');
+                    if (i == 5){
+                        tr.setAttribute('class', "row-block");
+                    }
+
+                    const rowfragment = document.createDocumentFragment();
+                    const FilterRowData = FilterData.filter(item => item.time == indexTime[i]);
+
+                    const cols = FilterRowData.length;
+
+                    for (let j = 0; j < cols; j++){
+                        const td = document.createElement('td');
+                        td.setAttribute('id', FilterRowData[j].schedule_cell+"td");
+
+                        rowfragment.appendChild(td);
+                    }
+                    tr.appendChild(rowfragment);
+                    tbody.appendChild(tr);
+                }
+                table.appendChild(tbody);
+            }
+            
+            
+            
         }
     })
     .catch(error => {
